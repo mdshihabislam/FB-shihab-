bde4129beb85f025963c9aa34e6cd09f02dbcb94
mdshihabislam <126655823+mdshihabislam@users.noreply.github.com>
2023-03-07 10:13:08 +0600

    Add files via upload

---
+++ FB(shihab).py
@@ -0,0 +1,55 @@
+#md shihab islam
+B='\033[1;94m'
+R='\033[1;91m'
+G='\033[1;92m'
+W='\033[1;97m'
+S='\033[1;96m'
+P='\033[1;95m'
+Y='\033[1;93m'
+print("""\033[1;91m==============================================================================
+""")
+print("""\033[1;92mFFFFFFFFFFFFFFFFFFFFFF     BBBBBBBBBBBBBBBBB
+F::::::::::::::::::::F     B::::::::::::::::B
+F::::::::::::::::::::F     B::::::BBBBBB:::::B
+FF::::::FFFFFFFFF::::F     BB:::::B     B:::::B
+  F:::::F       FFFFFF       B::::B     B:::::B
+  F:::::F                    B::::B     B:::::B
+  F::::::FFFFFFFFFF          B::::BBBBBB:::::B
+  F:::::::::::::::F          B:::::::::::::BB
+  F:::::::::::::::F          B::::BBBBBB:::::B
+  F::::::FFFFFFFFFF          B::::B     B:::::B
+  F:::::F                    B::::B     B:::::B
+  F:::::F                    B::::B     B:::::B
+FF:::::::FF                BB:::::BBBBBB::::::B
+F::::::::FF                B:::::::::::::::::B
+F::::::::FF                B::::::::::::::::B
+FFFFFFFFFFF                BBBBBBBBBBBBBBBBB
+""")
+print()
+print("""\033[1;91m===============================================================================
+""")
+print("""\033[1;93m
+★DEVELOPER  : MD. SHIHAB ISLAM  """)
+print("""\033[1;93m★Github     : https://github.com/mdshihabislam """)
+print("""\033[1;93m★Facebook   : https://facebook.com/groups/156030057305763/  """)
+print()
+print("""\033[1;91m===============================================================================""")
+print("""❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌""")
+print("""★ ★  ★  ★  ★  ★  ★ ★  ★ ★  ★ ★  ★ ★  ★ ★  ★  ★ ★  ★ ★  ★ ★  ★  ★ ★  ★ ★★ ★ ★ ★ """)
+
+
+
+
+print()
+print()
+print()
+print("""\033[1;93m""")
+p=input("""  FacebookUserId and Password cloing start   :  type inter  """)
+
+a=100090388031133
+b=360450
+while a <=100090388059899:
+	
+	print('\033[1;92m====FacebookUserID===>','\033[1;93m',  a,'\033[1;92m====Password=====>','\033[1;93m',b)
+	a=a+1
+	b=b+1
